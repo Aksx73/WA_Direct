e9d1bbbe0124d07c20bf2edd219a05d20670a025
Akshay <akshaynighot73@gmail.com>
2025-07-08 20:56:12 +0530

    add compose theme files

---
+++ app/src/main/java/com/android/waintent/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.android.waintent.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val md_theme_light_primary = Color(0xFF006D3B)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+val md_theme_light_background = Color(0xFFFBFDF7)
+val md_theme_light_onBackground = Color(0xFF191C1A)
+
+val md_theme_dark_primary = Color(0xFF5BDB8B)
+val md_theme_dark_onPrimary = Color(0xFF00391C)
+val md_theme_dark_background = Color(0xFF191C1A)
+val md_theme_dark_onBackground = Color(0xFFE1E3DE)
